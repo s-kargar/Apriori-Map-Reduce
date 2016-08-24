48abec32b284e5e84e086c3e810fb84c494a299b
Sina-kargar <cna.kargar@gmail.com>
2016-08-24 16:36:51 +0200

    Add files via upload

--- BDA-script.sh
+++ BDA-script.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+
 #HDFS Directories --> ENTER HERE
-HDFS_INPUT_DIR="hdfs://cluster-1-m:8020/input"
-HDFS_OUTPUT_DIR="hdfs://cluster-1-m:8020/output"
-SUPPORT_VALUE="400000"
+HDFS_INPUT_DIR="./input"
+HDFS_OUTPUT_DIR="./output"
+SUPPORT="0.98"
 NUM_REDUCERS="1"
 ###################################
 #Setting variables
+
+HADOOP="$( which hadoop )"
+HDFS="$(which hdfs)"
+
 CURRENT_DIR=$(pwd)
 SOURCE_DIR=$CURRENT_DIR
 LOCAL_OUTPUT_DIR=$SOURCE_DIR
-#DATE=`date +%H-%M`
-LOCAL_OUTPUT_FILE="$LOCAL_OUTPUT_DIR/output-sup-$SUPPORT_VALUE-red-$NUM_REDUCERS.txt"
+
+REC_COUNT=`$HDFS dfs -cat $HDFS_INPUT_DIR/* | wc -l`
+SUPPORT_COUNT=`perl -E "say $SUPPORT*$REC_COUNT"`
+
+LOCAL_OUTPUT_FILE="$LOCAL_OUTPUT_DIR/output-sup-$SUPPORT_COUNT-red-$NUM_REDUCERS.txt"
 LOCAL_OUTPUT_FILE_TEMP="$LOCAL_OUTPUT_DIR/output_temp.txt"
 [ -e $LOCAL_OUTPUT_FILE_TEMP ] && rm -f $LOCAL_OUTPUT_FILE_TEMP
 
@@ -19,13 +27,13 @@ SORT_SCRIPT="$SOURCE_DIR/py-sort.py"
 HDFS_CACHE_DIR="$HDFS_OUTPUT_DIR/cache"
 CACHE_FILE="$HDFS_CACHE_DIR/cache.txt"
 
-HADOOP="$( which hadoop )"
-HDFS="$(which hdfs)"
+
 TO_CACHE="$HDFS dfs -put -f $LOCAL_OUTPUT_FILE $CACHE_FILE"
 SORT_OUTPUT="python $SORT_SCRIPT $LOCAL_OUTPUT_FILE_TEMP $LOCAL_OUTPUT_FILE"
-LOG_FILE="log-sup-$SUPPORT_VALUE-red-$NUM_REDUCERS.txt"
+LOG_FILE="log-sup-$SUPPORT_COUNT-red-$NUM_REDUCERS.txt"
+
 {
-echo "Apriroi Algorithm --> support threshold= $SUPPORT_VALUE, number of reducers= $NUM_REDUCERS"
+echo "Apriroi Algorithm --> support threshold= $SUPPORT, support count=$SUPPORT_COUNT, number of reducers= $NUM_REDUCERS"
 ##hdfs output and cache directory
 $HDFS dfs -mkdir -p $HDFS_CACHE_DIR
 ##################################
@@ -34,7 +42,7 @@ $HDFS dfs -mkdir -p $HDFS_CACHE_DIR
 
 ITEMSET_SIZE="1"
 start_time1=`date +%s`
-$HADOOP jar $JAR_FILE $HDFS_INPUT_DIR $HDFS_OUTPUT_DIR $SUPPORT_VALUE $ITEMSET_SIZE $NUM_REDUCERS
+$HADOOP jar $JAR_FILE $HDFS_INPUT_DIR $HDFS_OUTPUT_DIR $SUPPORT_COUNT $ITEMSET_SIZE $NUM_REDUCERS
 $HDFS dfs -getmerge $HDFS_OUTPUT_DIR/$ITEMSET_SIZE $LOCAL_OUTPUT_FILE_TEMP
 $HDFS dfs -rm -r -skipTrash $HDFS_OUTPUT_DIR/$ITEMSET_SIZE
 $SORT_OUTPUT
@@ -43,17 +51,17 @@ end_time=`date +%s`
 echo "$ITEMSET_SIZE-itemsets time = $((end_time-start_time1))seconds"
 echo "---------------Next itemset--------------"
 # K-frequent itemset
-for i in `seq 2 10`;
+for i in `seq 2 1000`;
 	do
 	let ITEMSET_SIZE=$i
 	start_time2=`date +%s`
-	$HADOOP jar $JAR_FILE $HDFS_INPUT_DIR $HDFS_OUTPUT_DIR $SUPPORT_VALUE $ITEMSET_SIZE $NUM_REDUCERS
+	$HADOOP jar $JAR_FILE $HDFS_INPUT_DIR $HDFS_OUTPUT_DIR $SUPPORT_COUNT $ITEMSET_SIZE $NUM_REDUCERS
 	$HDFS dfs -getmerge $HDFS_OUTPUT_DIR/$ITEMSET_SIZE $LOCAL_OUTPUT_FILE_TEMP
 	$HDFS dfs -rm -r -skipTrash $HDFS_OUTPUT_DIR/$ITEMSET_SIZE
 	[ -s $LOCAL_OUTPUT_FILE_TEMP ]||{
 		let end_time=`date +%s`
 		echo "NO FREQUENT ITEMSET SIZE=$ITEMSET_SIZE WAS FOUND"
-		echo "total running time =$((end_time-start_time1)) seconds, SUPPORT=$SUPPORT_VALUE , number of reducers=$NUM_REDUCERS"
+		echo "total running time =$((end_time-start_time1)) seconds, SUPPORT=$SUPPORT_COUNT , number of reducers=$NUM_REDUCERS"
 		break
 		}
 	$SORT_OUTPUT
